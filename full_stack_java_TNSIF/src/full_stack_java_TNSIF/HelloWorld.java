c26045b8a14662fd6053ada3d61dbd88e447bb51
prana <prana@DRAX>
2024-07-11 10:46:22 +0530

    My First Commit

---
+++ full_stack_java_TNSIF/src/full_stack_java_TNSIF/HelloWorld.java
@@ -0,0 +1,11 @@
+//printing message on console.
+package full_stack_java_TNSIF;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
